60edfd576f655ad2ebedd3fb3193850fc64aca20
LiamJMiller <liamjmiller.15@gmail.com>
2023-06-01 11:35:36 +0100

    LM 404 page edit

--- src/Pages/DeadEnd.js
+++ src/Pages/DeadEnd.js
@@ -1,5 +1,10 @@
 const DeadEnd = () => {
-  return <h2>Sorry Nothing to be found here</h2>;
+  return (
+    <>
+      <h2>Sorry Nothing to be found here</h2>
+      <h3>Yet!</h3>
+    </>
+  );
 };
 
 export default DeadEnd;
